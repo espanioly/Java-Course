3aba34a366999379e3e4deaa2c3316c0667b2a6d
Espanioly <espanioly.1@osu.edu>
2025-02-18 18:34:48 +0200

    Just doing some interview questions

--- src/com/sam/e/Main.java
+++ src/com/sam/e/Main.java
@@ -1,10 +1,34 @@
 package com.sam.e;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import static com.sam.e.Print.*;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
+    public String[] sortArray(String[] arr){
+        ArrayList<String> green = new ArrayList<>();
+        ArrayList<String> yellow = new ArrayList<>();
+        ArrayList<String> red = new ArrayList<>();
+        for (int i = 0; i < arr.length; i++){
+            if(arr[i].equalsIgnoreCase("red")){
+                red.add(arr[i]);
+            } else if (arr[i].equalsIgnoreCase("green")) {
+                    green.add(arr[i]);
+            }else if (arr[i].equalsIgnoreCase("yellow")){
+                yellow.add(arr[i]);
+            }else{
+                printeln("ERRORRRRR WRONG INPUT!!!!");
+            }
+        }
+        red.addAll(green);
+        red.addAll(yellow);
+        return red.toArray(new String[arr.length]);
+        // Needs testing
+    }
+
     public static void main(String[] args) {
-        println("Hello World!");
+
+
     }
 }
